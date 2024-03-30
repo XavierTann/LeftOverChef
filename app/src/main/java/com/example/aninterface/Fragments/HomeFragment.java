138c3e5bc4a5e184ff89c64fbfa373973ac6f468
yuhueng <144983073+yuhueng@users.noreply.github.com>
2024-03-30 19:29:28 +0800

    Added Flexbox

--- app/src/main/java/com/example/aninterface/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/aninterface/Fragments/HomeFragment.java
@@ -19,8 +19,6 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
---
+++ app/src/main/java/com/example/aninterface/Fragments/pantryIngredientItem.java
@@ -0,0 +1,53 @@
+package com.example.aninterface.Fragments;
+
+public class pantryIngredientItem {
+
+    private String ingredientName;
+    private String ingredientAmount;
+    private String ingredientUnit;
+    private boolean isSelected;
+
+    public pantryIngredientItem(String ingredientName, String ingredientAmount, String ingredientUnit, boolean isSelected){
+        this.ingredientName = ingredientName;
+        this.ingredientAmount = ingredientAmount;
+        this.ingredientUnit = ingredientUnit;
+        this.isSelected = isSelected;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public String getIngredientAmount() {
+        return ingredientAmount;
+    }
+
+    public void setIngredientAmount(String ingredientAmount) {
+        this.ingredientAmount = ingredientAmount;
+    }
+
+    public String getIngredientUnit() {
+        return ingredientUnit;
+    }
+
+    public void setIngredientUnit(String ingredientUnit) {
+        this.ingredientUnit = ingredientUnit;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+
+    @Override
+    public String toString(){
+        return "pantryIngredientItem{" + "name=" + ingredientName + '\'' + ", isSelected=" + isSelected + '}' ;
+    }
+}
