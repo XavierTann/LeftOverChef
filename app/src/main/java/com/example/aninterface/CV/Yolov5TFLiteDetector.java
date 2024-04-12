21581863d2d23f138bc89c76b925ff7ba8e6f7d8
Jithin Bathula <90094241+JithinBathula@users.noreply.github.com>
2024-04-12 13:42:43 +0800

    Update Yolov5TFLiteDetector.java

--- app/src/main/java/com/example/aninterface/CV/Yolov5TFLiteDetector.java
+++ app/src/main/java/com/example/aninterface/CV/Yolov5TFLiteDetector.java
@@ -1,3 +1,7 @@
+// This code unpacks the TFlite Model and executes the Non-Max Suppression (NMS) algorithm.
+// Adapted from an existing GitHub repository.
+//GitHub Repository URL: https://github.com/yyccR/yolov5-tflite-android/tree/master 
+
 package com.example.aninterface.CV;
 
 import android.content.Context;
@@ -73,11 +77,6 @@ public class Yolov5TFLiteDetector {
     public Size getInputSize(){return this.INPNUT_SIZE;}
     public int[] getOutputSize(){return this.OUTPUT_SIZE;}
 
-    /**
-     * 初始化模型, 可以通过 addNNApiDelegate(), addGPUDelegate()提前加载相应代理
-     *
-     * @param activity
-     */
     public void initialModel(Context activity) {
         // Initialise the model
         try {
@@ -96,18 +95,10 @@ public class Yolov5TFLiteDetector {
         }
     }
 
-    /**
-     * 检测步骤
-     *
-     * @param bitmap
-     * @return
-     */
     public ArrayList<Recognition> detect(Bitmap bitmap) {
 
         BITMAP_HEIGHT = bitmap.getHeight();
         BITMAP_WIDTH = bitmap.getWidth();
-
-        // yolov5s-tflite的输入是:[1, 320, 320,3], 摄像头每一帧图片需要resize,再归一化
         TensorImage yolov5sTfliteInput;
         ImageProcessor imageProcessor;
         if(IS_INT8){
@@ -131,23 +122,17 @@ public class Yolov5TFLiteDetector {
         yolov5sTfliteInput.load(bitmap);
         yolov5sTfliteInput = imageProcessor.process(yolov5sTfliteInput);
 
-
-        // yolov5s-tflite的输出是:[1, 6300, 85], 可以从v5的GitHub release处找到相关tflite模型, 输出是[0,1], 处理到320.
         TensorBuffer probabilityBuffer;
         if(IS_INT8){
             probabilityBuffer = TensorBuffer.createFixedSize(OUTPUT_SIZE, DataType.UINT8);
         }else{
             probabilityBuffer = TensorBuffer.createFixedSize(OUTPUT_SIZE, DataType.FLOAT32);
         }
-
-        // 推理计算
         if (null != tflite) {
-            // 这里tflite默认会加一个batch=1的纬度
             Log.d(">>> ", yolov5sTfliteInput.getTensorBuffer().getFlatSize() + " " + probabilityBuffer.getFlatSize());
             tflite.run(yolov5sTfliteInput.getBuffer(), probabilityBuffer.getBuffer());
         }
 
-        // 这里输出反量化,需要是模型tflite.run之后执行.
         if(IS_INT8){
             TensorProcessor tensorProcessor = new TensorProcessor.Builder()
                     .add(new DequantizeOp(output5SINT8QuantParams.getZeroPoint(), output5SINT8QuantParams.getScale()))
@@ -155,13 +140,10 @@ public class Yolov5TFLiteDetector {
             probabilityBuffer = tensorProcessor.process(probabilityBuffer);
         }
 
-        // 输出数据被平铺了出来
         float[] recognitionArray = probabilityBuffer.getFloatArray();
-        // 这里将flatten的数组重新解析(xywh,obj,classes).
         ArrayList<Recognition> allRecognitions = new ArrayList<>();
         for (int i = 0; i < OUTPUT_SIZE[1]; i++) {
             int gridStride = i * OUTPUT_SIZE[2];
-            // 由于yolov5作者在导出tflite的时候对输出除以了image size, 所以这里需要乘回去
             float x = recognitionArray[0 + gridStride] * BITMAP_WIDTH;
             float y = recognitionArray[1 + gridStride] * BITMAP_HEIGHT;
             float w = recognitionArray[2 + gridStride] * BITMAP_WIDTH;
@@ -172,9 +154,6 @@ public class Yolov5TFLiteDetector {
             int ymax = (int) Math.min(BITMAP_HEIGHT, y + h / 2.);
             float confidence = recognitionArray[4 + gridStride];
             float[] classScores = Arrays.copyOfRange(recognitionArray, 5 + gridStride, this.OUTPUT_SIZE[2] + gridStride);
-//            if(i % 1000 == 0){
-//                Log.i("tfliteSupport","x,y,w,h,conf:"+x+","+y+","+w+","+h+","+confidence);
-//            }
             int labelId = 0;
             float maxLabelScores = 0.f;
             for (int j = 0; j < classScores.length; j++) {
@@ -194,14 +173,8 @@ public class Yolov5TFLiteDetector {
             allRecognitions.add(
                     r);
         }
-//        Log.i("tfliteSupport", "recognize data size: "+allRecognitions.size());
-
-        // 非极大抑制输出
         ArrayList<Recognition> nmsRecognitions = nms(allRecognitions);
-        // 第二次非极大抑制, 过滤那些同个目标识别到2个以上目标边框为不同类别的
         ArrayList<Recognition> nmsFilterBoxDuplicationRecognitions = nmsAllClass(nmsRecognitions);
-
-        // 更新label信息
         for(Recognition recognition : nmsFilterBoxDuplicationRecognitions){
             int labelId = recognition.getLabelId();
             String labelName = associatedAxisLabels.get(labelId);
@@ -210,42 +183,26 @@ public class Yolov5TFLiteDetector {
 
         return nmsFilterBoxDuplicationRecognitions;
     }
-
-    /**
-     * 非极大抑制
-     *
-     * @param allRecognitions
-     * @return
-     */
     protected ArrayList<Recognition> nms(ArrayList<Recognition> allRecognitions) {
         ArrayList<Recognition> nmsRecognitions = new ArrayList<Recognition>();
-
-        // 遍历每个类别, 在每个类别下做nms
         for (int i = 0; i < OUTPUT_SIZE[2]-5; i++) {
-            // 这里为每个类别做一个队列, 把labelScore高的排前面
             PriorityQueue<Recognition> pq =
                     new PriorityQueue<Recognition>(
                             6300,
                             new Comparator<Recognition>() {
                                 @Override
                                 public int compare(final Recognition l, final Recognition r) {
-                                    // Intentionally reversed to put high confidence at the head of the queue.
                                     return Float.compare(r.getConfidence(), l.getConfidence());
                                 }
                             });
 
-            // 相同类别的过滤出来, 且obj要大于设定的阈值
             for (int j = 0; j < allRecognitions.size(); ++j) {
-//                if (allRecognitions.get(j).getLabelId() == i) {
                 if (allRecognitions.get(j).getLabelId() == i && allRecognitions.get(j).getConfidence() > DETECT_THRESHOLD) {
                     pq.add(allRecognitions.get(j));
-//                    Log.i("tfliteSupport", allRecognitions.get(j).toString());
                 }
             }
 
-            // nms循环遍历
             while (pq.size() > 0) {
-                // 概率最大的先拿出来
                 Recognition[] a = new Recognition[pq.size()];
                 Recognition[] detections = pq.toArray(a);
                 Recognition max = detections[0];
@@ -262,13 +219,7 @@ public class Yolov5TFLiteDetector {
         }
         return nmsRecognitions;
     }
-
-    /**
-     * 对所有数据不区分类别做非极大抑制
-     *
-     * @param allRecognitions
-     * @return
-     */
+    
     protected ArrayList<Recognition> nmsAllClass(ArrayList<Recognition> allRecognitions) {
         ArrayList<Recognition> nmsRecognitions = new ArrayList<Recognition>();
 
@@ -283,7 +234,6 @@ public class Yolov5TFLiteDetector {
                             }
                         });
 
-        // 相同类别的过滤出来, 且obj要大于设定的阈值
         for (int j = 0; j < allRecognitions.size(); ++j) {
             if (allRecognitions.get(j).getConfidence() > DETECT_THRESHOLD) {
                 pq.add(allRecognitions.get(j));
@@ -291,7 +241,6 @@ public class Yolov5TFLiteDetector {
         }
 
         while (pq.size() > 0) {
-            // 概率最大的先拿出来
             Recognition[] a = new Recognition[pq.size()];
             Recognition[] detections = pq.toArray(a);
             Recognition max = detections[0];
@@ -335,30 +284,17 @@ public class Yolov5TFLiteDetector {
         return u;
     }
 
-    /**
-     * 添加NNapi代理
-     */
+
     public void addNNApiDelegate() {
         NnApiDelegate nnApiDelegate = null;
         // Initialize interpreter with NNAPI delegate for Android Pie or above
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-//            NnApiDelegate.Options nnApiOptions = new NnApiDelegate.Options();
-//            nnApiOptions.setAllowFp16(true);
-//            nnApiOptions.setUseNnapiCpu(true);
-            //ANEURALNETWORKS_PREFER_LOW_POWER：倾向于以最大限度减少电池消耗的方式执行。这种设置适合经常执行的编译。
-            //ANEURALNETWORKS_PREFER_FAST_SINGLE_ANSWER：倾向于尽快返回单个答案，即使这会耗费更多电量。这是默认值。
-            //ANEURALNETWORKS_PREFER_SUSTAINED_SPEED：倾向于最大限度地提高连续帧的吞吐量，例如，在处理来自相机的连续帧时。
-//            nnApiOptions.setExecutionPreference(NnApiDelegate.Options.EXECUTION_PREFERENCE_SUSTAINED_SPEED);
-//            nnApiDelegate = new NnApiDelegate(nnApiOptions);
             nnApiDelegate = new NnApiDelegate();
             options.addDelegate(nnApiDelegate);
             Log.i("tfliteSupport", "using nnapi delegate.");
         }
     }
 
-    /**
-     * 添加GPU代理
-     */
     public void addGPUDelegate() {
         CompatibilityList compatibilityList = new CompatibilityList();
         if(compatibilityList.isDelegateSupportedOnThisDevice()){
@@ -371,10 +307,6 @@ public class Yolov5TFLiteDetector {
         }
     }
 
-    /**
-     * 添加线程数
-     * @param thread
-     */
     public void addThread(int thread) {
         options.setNumThreads(thread);
     }
